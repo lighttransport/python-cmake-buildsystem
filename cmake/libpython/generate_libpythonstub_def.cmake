88f058a6d951c42acbb12d1b4cf2fe0241a51210
Dan Dees <dand@appsmiths.com>
2017-08-28 12:35:33 +0700

    BUG - def convert loop skipping last line

--- cmake/libpython/generate_libpythonstub_def.cmake
+++ cmake/libpython/generate_libpythonstub_def.cmake
@@ -5,12 +5,19 @@ foreach(varname INPUT_DEF_FILE OUTPUT_DEF_FILE)
   endif()
 endforeach()
 
-file(STRINGS ${INPUT_DEF_FILE} def_lines REGEX "^  (.+)=.+$")
-set(stub_def_lines "EXPORTS\n")
+# read in strings with "="
+file(STRINGS ${INPUT_DEF_FILE} def_lines REGEX ".*=.*")
+
+# initialize output file
+file(WRITE ${OUTPUT_DEF_FILE} "EXPORTS")
+
+# run through each line
 foreach(line IN LISTS def_lines)
-  string(REGEX MATCH "^  (.+)=.+$" output ${line})
-  if(NOT "${output}" STREQUAL "")
-    set(stub_def_lines "${stub_def_lines}${CMAKE_MATCH_1}\n")
-  endif()
-endforeach()
-file(WRITE ${OUTPUT_DEF_FILE} "${stub_def_lines}")
+
+  # kill "=..." in the line
+  string(REGEX REPLACE "=.+" "" updated_line ${line})
+
+  # stick on end of file
+  file(APPEND ${OUTPUT_DEF_FILE} "${updated_line}\n")
+
+endforeach(line IN def_lines)
