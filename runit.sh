27fce228d5f9386efcdcbdfa8c469b8857d96cb4
Dan Dees <dand@appsmiths.com>
2019-03-14 19:19:07 -0500

    runit

--- runit.sh
+++ runit.sh
@@ -1,2 +1,6 @@
-python run_py_ver.py ${ASV_PLAT_PORTS}/python setup_py_env.py
-# python setup_py_env.py ${ASV_PLAT_PORTS}/python
+declare pydir='python-debug'
+declare pydir='python'
+declare -r PR="${ASV_PLAT_PORTS}/${pydir}"
+
+python run_py_ver.py ${PR} setup_py_env.py
+# python setup_py_env.py ${PR}
